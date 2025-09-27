8f3e5754d5784b6c3f117af050f30b9a16112fe8
inngok <ngocnguyen120305@gmail.com>
2025-09-27 13:01:36 +0700

    fix: update routes + header/footer

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,8 +2,8 @@ export default function Header() {
   return (
     <header className="bg-[#0099CF] text-white py-5 shadow-md">
       <div className="container mx-auto text-center">
-        <h1 className="text-3xl font-extrabold tracking-wide">Quán Cơm </h1>
-        <p className="mt-1 text-s opacity-90">
+        <h1 className="text-3xl font-extrabold tracking-wide">Quán Cơm</h1>
+        <p className="mt-1 text-sm opacity-90">
           Đặt món qua Zalo: <span className="font-semibold">0123456789</span>
         </p>
       </div>
